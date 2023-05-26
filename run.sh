79aac4254c1e60da52cfa25bf0190342235f1b96
Matteo Rapa <46799259+matteorapa@users.noreply.github.com>
2023-05-26 17:37:54 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=4
 #SBATCH --cpus-per-task=48
-#SBATCH --gpus=3
+#SBATCH --gpus=1
 #SBATCH --partition=gpu
 #SBATCH --time=60:00:00
 #SBATCH --mail-type=BEGIN,END
@@ -18,7 +18,6 @@ python main.py --model resnet50 --epoch 10 --out ../results/conv_only/ --prune 0
 python main.py --model resnet50 --epoch 10 --out ../results/conv_only/ --prune 0.05
 python main.py --model resnet50 --epoch 10 --out ../results/conv_only/ --prune 0.10
 python main.py --model resnet50 --epoch 10 --out ../results/conv_only/ --prune 0.15
-python main.py --model resnet50 --epoch 10 --out ../results/conv_only/ --prune 0.15
-python main.py --model resnet50 --epoch 10 --out ../results/conv_only/ --prune 0.20
-python main.py --model resnet50 --epoch 10 --out ../results/conv_only/ --prune 0.25
-python main.py --model resnet50 --epoch 10 --out ../results/conv_only/ --prune 0.30
+# python main.py --model resnet50 --epoch 10 --out ../results/conv_only/ --prune 0.20
+# python main.py --model resnet50 --epoch 10 --out ../results/conv_only/ --prune 0.25
+# python main.py --model resnet50 --epoch 10 --out ../results/conv_only/ --prune 0.30
