88920cd645171901bce3bd4d581801490b7c6981
Matteo Rapa <46799259+matteorapa@users.noreply.github.com>
2023-05-26 16:05:59 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@
 source /home/matteor/anaconda3/etc/profile.d/conda.sh
 conda activate base
 
-cd $HOME/msc-thesis-self-adaptive-neural-networks
+cd $HOME/msc-thesis-self-adaptive-neural-networks/src
 
 python main.py --model resnet50 --epoch 10 --out ../results/ --prune 0
 python main.py --model resnet50 --epoch 10 --out ../results/ --prune 0.05
