28817c503887c683a579aa33f0fb3be7e699e040
Matteo Rapa <46799259+matteorapa@users.noreply.github.com>
2023-05-29 15:39:09 +0200

    Update paths.py

--- src/paths.py
+++ src/paths.py
@@ -1,3 +1,3 @@
 VALIDATION_DIR = '../data/val'
-TRAIN_DIR = '../data/ILSVRC2012_training'
+TRAIN_DIR = '../data/train/ILSVRC2012_img_train.tar'
 VALIDATION_LABELS_DIR = '../data/ILSVRC2012_validation_ground_truth.txt'
