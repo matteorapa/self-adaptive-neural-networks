4ec7610fdb7db8be01e99d19b2ebaf4e392dc7da
Matteo Rapa <46799259+matteorapa@users.noreply.github.com>
2023-05-29 16:46:51 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -36,176 +36,6 @@ def switch(model):
     elif model == "vgg":
         return torchvision.models.vgg19(pretrained=True)
 
-class TARDataset(torch.utils.data.Dataset):
-    """Dataset for packed ZIP to pass to a PyTorch dataloader
-
-    Args:
-        path (str): location where the images are stored on disk
-        transform (obj): torchvision.transforms object or None
-        load_encoded (bool): whether the images within the .tar file are encoded or saved as bytes directly
-        label_file (str): path to save the cached getmembers() output as this may take a while for larger dataset
-    Returns:
-        torch Dataset: to pass to a dataloader
-    """
-
-    def __init__(self, path, transform=None, load_encoded=False, label_file=None):
-        print(f"Initializing TAR dataset for: {path}")
-        self.path = path
-        self.transform = transform
-        self.load_encoded = load_encoded
-
-        # First uncompress because .gz cannot be read in parallel
-        if self.path.endswith(".tar.gz"):
-            print(
-                "WARNING: tar file is compressed which drastically impacts performance -> gunzip -k <file>"
-            )
-
-        # TAR is not thread-safe so give file handle to each worker
-        worker = torch.utils.data.get_worker_info()
-        worker = worker.id if worker else None
-        self.tar_handle = {worker: tarfile.open(path)}
-
-        # Store headers of all files and folders by name
-        self.members = sorted(self.tar_handle[worker].getmembers(), key=lambda m: m.name)
-
-        # store headers of all files and folders by name
-        if label_file:
-            with open(label_file, "rb") as fp:
-                self.members_by_name = pickle.load(fp)
-
-        else:
-            # get.members() takes very long for larger TAR archives so cache the members in a byte file
-            self.members_by_name = {
-                m.name: m
-                for m in sorted(
-                    self.tar_handle[worker].getmembers(), key=lambda m: m.name
-                )
-            }
-            with open(os.path.join(Path(path).parent, "members"), "wb") as fp:
-                pickle.dump(self.members_by_name, fp)
-            print(
-                f"Finished create a members file. Please add the following path to the init as label_file next time: ",
-                str(Path(path).parent) + "members",
-            )
-
-        self._get_all_samples()
-        label_fname = None  # or "String"
-
-        if len(self.label_fname) >= 1 and label_fname is not None:
-            self._parse_label_file(worker, label_fname)
-        else:
-            self._get_filler_labels()
-
-    def _get_all_samples(self, label_exts=(".json")):
-        """Sort labels and images from the names
-        Args:
-            label_exts: the file extensions which will be categorized as labels
-        Returns:
-            None: filled lists of self.samples and self.label_fname
-        """
-        self.members_by_name = {m.name: m for m in self.members}
-        self.samples = []
-        self.label_fname = []
-        PIL.Image.init()
-
-        for m in self.members_by_name.values():
-            m_name = m.name
-            if m_name.lower().endswith(tuple(PIL.Image.EXTENSION.keys())):
-                self.samples.append(m_name)
-            elif m_name.lower().endswith(label_exts):
-                self.label_fname.append(m_name)
-        self.file_ext = os.path.splitext(self.samples[0])[1]
-
-        self.length = len(self.samples)
-
-    def _parse_label_file(self, worker, label_fname=""):
-        """Sort labels and images from the names
-        Args:
-            worker (int): worker id
-            label_fname (str): hardcoded label file name
-        Returns:
-            None: parsed labels
-        """
-        # If no hardcoded label filename is given, use the first one from the list by _get_all_samples()
-        if label_fname == "":
-            if len(self.label_fname) != 1:
-                print(
-                    f"WARNING: found {len(self.label_fname)} label files - using only the first"
-                )
-
-            label_fname = self.label_fname[0]
-
-        # Parse labels from json file
-        label_file = self.tar_handle[worker].extractfile(label_fname)
-        labels = json.load(label_file)["labels"]
-        labels = dict(sorted(labels, key=lambda item: item[1]))
-
-        labels = [labels[fname.replace("\\", "/")] for fname in self.samples]
-        self.labels = np.array(labels, dtype=np.uint8)
-
-    def _get_filler_labels(self):
-        # Placeholder for datasets with no labels like FFHQ
-        self.labels = [0] * self.length
-
-    def _get_file(self, fname):
-        """Retrieve file handle for a given file name within the ZIP file"""
-        worker = torch.utils.data.get_worker_info()
-        worker = worker.id if worker else None
-
-        if worker not in self.tar_handle:
-            self.tar_handle[worker] = tarfile.open(self.path)
-
-        return self.tar_handle[worker].extractfile(self.members_by_name[fname])
-
-    def _get_image(self, fname):
-        fname = self._get_file(fname)
-        jpgs = (".jpg", ".jpeg")
-        if not self.load_encoded:
-            # In case of having the image saved as bytes:
-            image = np.frombuffer(fname.read(), dtype=np.uint8).reshape(256, 256, 3)
-        else:
-            if self.file_ext.lower() == ".png" and pyspng:
-                image = pyspng.load(fname.read())
-            elif self.file_ext.lower() in jpgs and turbojpeg:
-                try:
-                    image = turbojpeg_decoder.decode(fname.read(), pixel_format=0)
-                except IOError:  # Catch jpgs which are actually encoded as PNG
-                    image = PIL.Image.open(fname).convert("RGB")
-            else:
-                image = PIL.Image.open(fname.read()).convert("RGB")
-
-        return image
-
-    def _get_label(self, index):
-        return self.labels[index]
-
-    def __getitem__(self, index):
-        fname_image = self.samples[index]
-        image = self._get_image(fname_image)
-        label = self._get_label(index)
-
-        if self.transform:
-            image = self.transform(image)
-        else:
-            image = to_tensor(image)
-
-        return image, label
-
-    def __len__(self):
-        return self.length
-
-    def __del__(self):
-        """Clean all file handles of the workers on exit"""
-        if hasattr(self, "tar_handle"):
-            for o in self.tar_handle.values():
-                o.close()
-
-    def __getstate__(self):
-        """Serialize without the ZipFile references, for multiprocessing compatibility"""
-        state = dict(self.__dict__)
-        state["tar_handle"] = {}
-        return state
-
 def tune(model: ResNet) -> ResNet:
     global best_prec1
 
@@ -228,12 +58,12 @@ def tune(model: ResNet) -> ResNet:
 
     print('Reading Tar archive headers...')
     t = time.time()
-    train_dataset = TarImageFolder(args.data, root_in_archive='ILSVRC12/train',
+    tar_train_dataset = TarImageFolder(args.data, root_in_archive='ILSVRC12/train',
         transform=transformations)
     print(f'Done in {float(time.time() - t):.1f} seconds.')
 
 
-    tar_train_dataset = TARDataset(path=TRAIN_DIR, transform=transformations, label_file=TRAIN_MEMBERS)
+    # tar_train_dataset = TARDataset(path=TRAIN_DIR, transform=transformations, label_file=TRAIN_MEMBERS)
     cudnn.benchmark = True
 
 
