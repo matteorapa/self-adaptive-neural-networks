f7c8cdf154dbd3ca503ffd89b6e8a4a0b25572b9
Matteo Rapa <46799259+matteorapa@users.noreply.github.com>
2023-05-31 10:54:11 +0200

    update path

--- src/paths.py
+++ src/paths.py
@@ -1,3 +1,3 @@
 VALIDATION_DIR = '../data/val'
-TRAIN_DIR = '../../../../scratch-shared/matteor/img_train'
+TRAIN_DIR = '../../img_train'
 VALIDATION_LABELS_DIR = '../data/ILSVRC2012_validation_ground_truth.txt'
--- src/search.py
+++ src/search.py
@@ -6,12 +6,12 @@ def print_files_and_folders(path):
     for root, directories, files in os.walk(path):
         # Print the current directory
         print(f"Directory: {root}")
-
-        # Print the subdirectories in the current directory
-        print("Subdirectories:")
-        for directory in directories:
-            print(directory)
-            break
+        break
+        # # Print the subdirectories in the current directory
+        # print("Subdirectories:")
+        # for directory in directories:
+        #     print(directory)
+        #     break
 
 
 
